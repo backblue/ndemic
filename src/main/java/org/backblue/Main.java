a039d22e31cfef4dd68eb93f4fdf517efdd98d1a
backblue <170589101+backblue@users.noreply.github.com>
2025-03-18 23:15:18 -0400

    Basic bot up & working

--- src/main/java/org/backblue/Main.java
+++ src/main/java/org/backblue/Main.java
@@ -1,17 +1,29 @@
 package org.backblue;
 
-
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.EnumSet;
 
 public class Main {
     public static void main(String[] args) {
 
-        JDA bot = JDABuilder.createLight("", EnumSet.allOf(GatewayIntent.class))
-                .build();
+        try {
+            String rawKey = Files.readString(Path.of("data/key.json"));
+            JSONObject key = new JSONObject(new JSONTokener(rawKey));
+
+            JDA bot = JDABuilder.createLight(key.getString("token"), EnumSet.allOf(GatewayIntent.class))
+                    .setActivity(Activity.customStatus("Facilitating requests"))
+                    .build();
+        } catch (Exception e) {
+            System.out.println("Error reading config files");
+        }
 
     }
 }
