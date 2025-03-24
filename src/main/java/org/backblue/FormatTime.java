0f6d42324bfb6be0df872011c25c1170528759a5
backblue <170589101+backblue@users.noreply.github.com>
2025-03-24 00:01:36 -0400

    New version

--- src/main/java/org/backblue/FormatTime.java
+++ src/main/java/org/backblue/FormatTime.java
@@ -56,4 +56,32 @@ public class FormatTime {
         return returnString;
     }
 
+    public static String formatTimeShort(String seconds) {
+        return formatTimeShort(Long.parseLong(seconds));
+    }
+
+    public static String formatTimeShort(Long seconds) {
+        if (seconds <= 0) {
+            return null;
+        }
+
+        String returnString = null;
+
+        long days = seconds / 86400;
+        long hours = (seconds % 86400) / 3600;
+        long minutes = (seconds % 3600) / 60;
+        long secs = seconds % 60;
+
+        if (days > 0) {
+            returnString = days + "d " + hours + "h " + minutes + "m " + secs + "s";
+        } else if (hours > 0) {
+            returnString = hours + "h " + minutes + "m " + secs + "s";
+        } else if (minutes > 0) {
+            returnString = minutes + "m " + secs + "s";
+        } else if (secs > 0) {
+            returnString = secs + "s";
+        }
+        return returnString;
+    }
+
 }
